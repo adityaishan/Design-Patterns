3357601454205a07fd525f16fffde5a53de68ee7
aditya Ishan <aditya.ishan27@gmail.com>
2019-05-06 18:24:56 +0530

    Update before_prototype.cpp

--- Creational Design Patterns/Prototype/Another Example/before_prototype.cpp
+++ Creational Design Patterns/Prototype/Another Example/before_prototype.cpp
@@ -7,7 +7,7 @@
 class Stooge
 {
    public:
-	 virtual void slap_stick() = 0;
+	     virtual void slap_stick() = 0;
 };
 
 class Larry: public Stooge
@@ -41,18 +41,17 @@ int main()
 {
 	
 	std::vector<std::unique_ptr<Stooge>> roles;
-
 	int choice;
 
 	while(true)
 	{
-		std::cout << "Larry(1) Moe(2) Curly(3) Exit(0): \n";
-		std::cin>>choice;
+      std::cout << "Larry(1) Moe(2) Curly(3) Exit(0): \n";
+      std::cin>>choice;
 
-		if(!choice) break;
-		else if(choice == 1) roles.push_back(std::make_unique<Larry>());
-	  else if(choice == 2) roles.push_back(std::make_unique<Moe>());
-	  else if(choice == 3) roles.push_back(std::make_unique<Curly>());
+      if(!choice) break;
+      else if(choice == 1) roles.push_back(std::make_unique<Larry>());
+      else if(choice == 2) roles.push_back(std::make_unique<Moe>());
+      else if(choice == 3) roles.push_back(std::make_unique<Curly>());
 	}
 
 	for(size_t i = 0; i != roles.size(); ++i)
